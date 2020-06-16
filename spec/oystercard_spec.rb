53fef23315d93a95b7cba8ff73a735dc7c346381
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-16 10:59:56 +0100

    Adds a let statement to DRY up code

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,17 +2,16 @@ require 'oystercard'
 
 describe OysterCard do
 
-    
+    let(:oystercard) { OysterCard.new }
+
 
     describe 'add' do
       it 'can have credit added to it' do
-        oystercard = OysterCard.new
         oystercard.add(5)
         expect(oystercard.balance).to eq 5
       end
 
       it 'can have no more than 90 pounds of credit added to it' do
-        oystercard = OysterCard.new
         oystercard.add(90)
         expect { oystercard.add(10) }.to raise_error "Unable to add credit. Maximum balance is 90"
       end 
