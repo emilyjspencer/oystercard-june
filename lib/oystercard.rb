514625a7b08b165998bfde88fce54a913f91ac05
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-16 11:01:17 +0100

    Refactors add() method to adhere to the Single Responsibility Principle. Creates private method maximum_balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,9 +7,15 @@ class OysterCard
     end 
 
     def add(credit)
-      fail "Unable to add credit. Maximum balance is 90" if @balance >= 90
+      fail "Unable to add credit. Maximum balance is 90" if maximum_balance
       @balance += credit
     end 
 
+    private 
+
+    def maximum_balance
+      return true if @balance >= 90
+    end 
+    
 
 end 
