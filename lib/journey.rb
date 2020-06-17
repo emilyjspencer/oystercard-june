57f073e499a00e0dfca2b18d1254bd5b331a506b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-17 20:33:29 +0100

    Adds journey class and specs

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,16 +1,37 @@
 class Journey
-    attr_reader :in_journey
+
+    MINIMUM_FARE = 1
+    PENALTY_FARE = 6
+
+    attr_reader :in_journey, :stations_passed, :fare
+
 
   def initialize
     @in_journey = false
+    @fare = PENALTY_FARE
+    @stations_passed = { entry: nil, exit: nil }
   end
 
-  def start
+  def start(entry)
     @in_journey = true
+    @stations_passed[:entry] = entry
   end 
 
-  def end
+  def end(exit)
     @in_journey = false
+    @stations_passed[:exit] = exit
+    calculate_fare
+  end 
+
+  def incomplete?
+    @stations_passed[:entry].nil?  || @stations_passed[:exit].nil?
+  end
+  
+ 
+
+  def calculate_fare
+    incomplete? ? @fare = PENALTY_FARE : @fare = MINIMUM_FARE
+    #@fare = PENALTY_FARE if incomplete?
   end 
 
 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,19 +5,56 @@ require 'journey'
 describe Journey do
 
   let(:journey) { Journey.new }
+  let(:station1) { double :station1 }
+  let(:station2) { double :station2 }
+  let(:full_journey) { { entry: station1, exit: station2 } }
 
   describe '#start' do
     it 'changes the in_journey status from false to true' do
-      journey.start
+      journey.start(station1)
       expect(journey.in_journey).to eq true
     end
+
+    it 'saves the entry station when starting a journey' do
+      journey.start(station1)
+      expect(journey.stations_passed[:entry]).to eq station1
+    end 
   end
 
   describe '#end' do
     it 'changes the in_journey status from true to false' do
-      journey.end
+      journey.end(station2)
       expect(journey.in_journey).to eq false
     end
+
+    it 'saves the exit station for full journey' do
+      journey.start(station1)
+      journey.end(station2)
+      expect(journey.stations_passed).to eq full_journey
+    end 
+
+    it 'recognizes incomplete journeys without a touch in' do
+      journey.end(station2)
+      expect(journey.incomplete?).to eq true
+    end
+
+    it 'recognizes incomplete journeys without a touch out' do
+      journey.start(station1)
+      expect(journey.incomplete?).to eq true
+    end 
+
+    it 'costs one pound for a complete journey' do
+      journey.start(station1)
+      journey.end(station2)
+      expect(journey.fare).to eq Journey::MINIMUM_FARE
+    end 
+
+    it 'costs six pounds for a journey if the user misses either tap in or out' do
+      journey.start(station1)
+      expect(journey.fare).to eq Journey::PENALTY_FARE
+    end 
+
+
   end 
 
       
