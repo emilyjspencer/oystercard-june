82e0777dd070277e955bd13b8b848ea33901d6d7
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-20 00:34:21 +0100

    Journey functionality extracted into Journey class. If a user tries to touch in or touch out two times in a row, the penalty fare is deducted from their balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,12 +8,13 @@ class OysterCard
     MINIMUM_BALANCE = 1
 
   
-    def initialize(journey = Journey.new)
-      @journey = journey
+    def initialize(journey_class: Journey)
+      @journey = nil
       @balance = 0
       @entry_station = entry_station
       @exit_station = exit_station
       @journey_history = []
+      @journey_class = journey_class
     end 
 
     def add(credit)
@@ -24,18 +25,15 @@ class OysterCard
 
     def touch_in(entry_station)
       fail "Unable to touch in. Minimum of 1 pound credit required" unless deceded?
-      fail "Already in journey" if in_journey?
-      @entry_station = entry_station 
-      @journey.start
+      touch_in_deduct unless @journey.nil?
+      start_journey(entry_station)
     end 
 
     def touch_out(exit_station)
-      fail "Card not in use" if !in_journey?
-      deduct(MINIMUM_BALANCE)
-      @exit_station = exit_station
-      @journey_history.push({ entry: @entry_station, exit: @exit_station })
-      @entry_station = nil
-      @journey.end
+      no_entry if @journey.nil?
+      @journey.end(exit_station)
+      deduct(@journey.fare)
+      save_journey
       
      
     end 
@@ -54,10 +52,28 @@ class OysterCard
       return true if @balance >= MINIMUM_BALANCE
     end 
 
-    private
 
     def deduct(credit)
       @balance -= credit
+    end 
+    
+    def touch_in_deduct
+      deduct(@journey.fare)
+      save_journey
+    end 
+
+    def no_entry
+      @journey = @journey_class.new
+    end 
+
+    def start_journey(entry_station)
+      @journey = @journey_class.new
+      @journey.start(entry_station)
+    end
+    
+    def save_journey
+      @journey_history.push(@journey)
+      @journey = nil
     end 
     
     
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,11 +2,20 @@ require 'oystercard'
 
 describe OysterCard do
 
-    let(:oystercard) { OysterCard.new }
+    let(:oystercard) { OysterCard.new(journey_class: journey_class) }
     let(:station1) { double :station1 }
     let(:station2) { double :station2 }
     let(:journey_made) { { entry: station1, exit: station2 }}
+    let(:journey) { double :journey, start: nil, end: nil, stations_passed: journey_made, fare: 1 }
+    let(:journey_class) { double :journey_class, new: journey }
 
+    let(:incomplete_journey1) { { entry: nil, exit: station2 } } # doesn't tap in 
+    let(:inc_journey1) { double :inc_journey, start: nil, end: nil, stations_passed: incomplete_journey1, fare: 6 }
+    let(:inc_journey_class1) { double :inc_journey_class, new: inc_journey1 }
+
+    let(:incomplete_journey2) { { entry: station1, exit: nil } } # doesn't tap out
+    let(:inc_journey2) { double :inc_journey, start: nil, end: nil, stations_passed: incomplete_journey2, fare: 6 }
+    let(:inc_journey_class2) { double :inc_journey_class, new: inc_journey2 }
 
     describe '#add' do
       it 'can have credit added to it' do
@@ -32,30 +41,34 @@ describe OysterCard do
 
       it 'raises an error if the card balance is less than 1 pound' do
         expect { oystercard.touch_in(station1) }.to raise_error "Unable to touch in. Minimum of 1 pound credit required"
-      end 
+      end
 
-      it 'raises an error if the user tries to touch in while already on a journey' do
+
+      #it 'remembers the entry station' do
+       # oystercard.add(20)
+        #oystercard.touch_in(station1)
+        #expect(oystercard.entry_station).to eq station1
+      #end 
+
+      it 'only touching in and not touching out is recorded as an incomplete journey' do
+        oystercard = OysterCard.new(journey_class: inc_journey_class1)
         oystercard.add(20)
         oystercard.touch_in(station1)
-        expect { oystercard.touch_in(station1) }.to raise_error "Already in journey"
+        oystercard.touch_in(station1)
+        expect(oystercard.journey_history[-1].stations_passed).to eq incomplete_journey1
       end 
 
-
-      it 'remembers the entry station' do
+      it 'only touching in triggers the penalty fare' do
+        oystercard = OysterCard.new(journey_class: inc_journey_class1)
         oystercard.add(20)
         oystercard.touch_in(station1)
-        expect(oystercard.entry_station).to eq station1
+        expect { oystercard.touch_in(station1) }.to change { oystercard.balance }.by(-6)
       end 
     end 
 
     describe '#touch_out' do
-     # it 'is no longer in_journey when touching out' do
-      #  oystercard.add(20)
-       # oystercard.touch_in(station1)
-        #oystercard.touch_out(station2)
-        #expect(oystercard.in_journey).to eq false
-      #end
-
+    
+  
       it 'deducts one pound from the credit upon touching out' do
         oystercard.add(20)
         oystercard.touch_in(station1)
@@ -68,6 +81,20 @@ describe OysterCard do
         oystercard.touch_out(station2)
         expect(oystercard.entry_station).to eq nil
       end 
+
+      it 'touching out but not touching in is registered as an incomplete journey' do
+        oystercard = OysterCard.new(journey_class: inc_journey_class2)
+        oystercard.add(20)
+        oystercard.touch_out(station2)
+        expect(oystercard.journey_history[-1].stations_passed).to eq incomplete_journey2
+      end 
+
+      it 'touching out but not touching in triggers the penalty fare' do
+        oystercard = OysterCard.new(journey_class: inc_journey_class2)
+        oystercard.add(20)
+        expect { oystercard.touch_out(station2) }.to change { oystercard.balance}.by(-6)
+      end 
+
     end 
 
     describe '#journey_history' do
@@ -75,7 +102,7 @@ describe OysterCard do
         oystercard.add(20)
         oystercard.touch_in(station1)
         oystercard.touch_out(station2)
-        expect(oystercard.journey_history).to include journey_made
+        expect(oystercard.journey_history[-1].stations_passed).to eq journey_made
       end
     end 
 
